5de86f86281d56aff1b7bcbbb3c9ee926d5a5e3d
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2023-04-02 09:29:16 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print("[*] koneksi anda buruk,silahkan refresh data anda.")
+	exit()
+	
+if __name__ == "__main__":
+	if "Indonesia" == fc:
+		__import__("regex").menu()
+	else:
+		__import__("regex").menu()
